6b35b88e56e332e5679e89c3620958ea30690295
matthew seyer <matthew.seyer@gmail.com>
2017-02-22 21:49:12 -0600

    to_hex_string now takes reference; New ByteArray struct for customizing buffer serialization output

--- src/librp/utils.rs
+++ src/librp/utils.rs
@@ -1,6 +1,21 @@
-pub fn to_hex_string(bytes: Vec<u8>) -> String {
+use std::fmt;
+
+pub fn to_hex_string(bytes: &Vec<u8>) -> String {
     let strs: Vec<String> = bytes.iter()
         .map(|b| format!("{:02X}", b))
         .collect();
     strs.join("")
+}
+
+// A type for larger byte arrays so we can customize serialization for output
+#[derive(Serialize)]
+pub struct ByteArray(pub Vec<u8>);
+impl fmt::Debug for ByteArray {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:?}",
+            to_hex_string(&self.0),
+        )
+    }
 }
