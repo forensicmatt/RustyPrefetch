61fad1a7b27f6394f463229bd2286c6be2221136
matthew seyer <matthew.seyer@gmail.com>
2017-04-08 16:02:29 -0500

    Added bitflags lib for flaged fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate log;
+#[macro_use] extern crate bitflags;
 extern crate rwinstructs;
 extern crate seek_bufread;
 extern crate serde;
